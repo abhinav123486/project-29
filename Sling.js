ed9dc4d4baaad31cb860072c410c2a245161ef4b
abhinav123486 <81365773+abhinav123486@users.noreply.github.com>
2021-06-25 07:45:56 +0530

    Add files via upload

---
+++ Sling.js
@@ -0,0 +1,33 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.1,
+            length: 1
+        }
+
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push()
+            strokeWeight(3);
+            line(pointA.position.x,pointA.position.y,pointB.position.x,pointB.position.y);
+            pop()
+        }
+    }
+    
+}
